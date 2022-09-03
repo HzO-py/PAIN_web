851537710a3d8396eb076330abd1548d5e13ae9b
ubuntu <ubuntu@localhost.localdomain>
2022-09-03 20:22:36 +0800

    v0.1 腾讯云部署

--- PAIN/wsgi.py
+++ PAIN/wsgi.py
@@ -7,7 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
+
+sys.path.append('/home/ubuntu/PAIN')
+sys.path.append('/home/ubuntu/anaconda3/envs/py36/Lib/site-packages')
 
 from django.core.wsgi import get_wsgi_application
 
